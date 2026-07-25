8ccef8b5ee5f1dc4d24f91c31cda203220c77b56
SantosRodrigo89 <rodrigo.v.santos89@gmail.com>
2026-07-24 22:25:51 -0300

    feat: enhance useProducts hook with loading and error states

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -4,13 +4,22 @@ import { productService } from '../domain/product/product.service'
 
 export function useProducts() {
   const [data, setData] = useState<Product[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<Error | null>(null)
 
   useEffect(() => {
     const loadProducts = async () => {
-      const response = await productService.getProducts()
-      setData(response.products)
+      try {
+        const response = await productService.getProducts()
+        setData(response.products)
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err)
+        }
+      } finally {
+        setIsLoading(false)
+      }
     }
-
     loadProducts()
   }, [])
 
